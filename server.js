ebde30eecbc6b3db736bc2081a1d9f0fd03fbbf4
jefazo-isc <pouputopou4@gmail.com>
2025-03-11 23:16:14 -0600

    Add files via upload
    el jefazo le cambio el puerto a 3000  en server. js    para que su sistema linux de cagada funcione bien

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //node Server.js
 
 // Parameters
-const port = 880; //Specify a port for our web server
+const port = 3000; //Specify a port for our web server
 const express = require('express'); //load express with the use of requireJs
 
 const app = express(); //Create an instance of the express library
